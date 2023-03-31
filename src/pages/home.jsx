63b9f095c42facdc0a9e37a2b2bc8c31e37369df
hollayemi18 <olayemio521@gmail.com>
2023-03-31 02:39:49 +0100

    hi

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
-import Accordion from '../componet/Accordion'
-import Navbar from '../componet/Navbar'
-import Hero from '../componet/Hero'
-import Image from '../componet/Image'
-import Features from '../componet/Features'
-import Testimony from '../componet/Testimony'
-import How from '../componet/How'
-import Footer from '../componet/Footer'
-
+//
 function Home() {
   return (
     <div>
-    <Navbar />
-    <Hero />
-    <Image />
-    <Features />
-    <How />
-    <Testimony />
-    <Accordion />
-    <Footer/>
-
+    <button>hi</button>
     </div>
   )
 }
