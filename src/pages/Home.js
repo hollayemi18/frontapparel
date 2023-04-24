47989b90f267cb6415d5a104f0eeed712d7a0e7d
hollayemi18 <olayemio521@gmail.com>
2023-04-24 02:28:38 +0100

    come

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "../componet/Navbar";
+import Accordion from "../componet/Accordion";
+import Features from "../componet/Features";
+import Footer from "../componet/Footer";
+import Hero from "../componet/Hero";
+import How from "../componet/How";
+import Image from "../componet/Image";
+import Testimony from "../componet/Testimony";
+function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Image />
+      <Features />
+      <How />
+      <Testimony />
+      <Accordion />
+      <Footer />
+    </main>
+  );
+}
+
+export default Home;
